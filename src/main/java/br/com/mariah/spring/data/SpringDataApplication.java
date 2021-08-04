25be6afd3ca468349354923869b804a1d0fdba21
matheus <matheusmartinsdacosta21@gmail.com>
2021-08-03 22:21:39 -0300

    Implementing command line runner to test respositories

--- src/main/java/br/com/mariah/spring/data/SpringDataApplication.java
+++ src/main/java/br/com/mariah/spring/data/SpringDataApplication.java
@@ -1,13 +1,31 @@
 package br.com.mariah.spring.data;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringDataApplication {
+import br.com.mariah.spring.data.model.Cargo;
+import br.com.mariah.spring.data.repository.CargoRepository;
 
+@SpringBootApplication
+public class SpringDataApplication implements CommandLineRunner {
+	
+	private final CargoRepository cargoRepository;
+	
+	public SpringDataApplication( CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDataApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("DESENVOLVEDOR JAVA PLENO");
+		
+		cargoRepository.save(cargo);
 	}
 
 }
--- src/main/java/br/com/mariah/spring/data/repository/CargoRepository.java
+++ src/main/java/br/com/mariah/spring/data/repository/CargoRepository.java
@@ -3,7 +3,9 @@ package br.com.mariah.spring.data.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.mariah.spring.data.model.Cargo;
+
 @Repository
-public interface CargoRepository extends CrudRepository {
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
 
 }
